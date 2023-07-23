dc8e84f3992447046b2d42bcfaafe69334fd2b03
OmarMWarraich <earthling986@gmail.com>
2023-07-23 12:40:36 +0500

    feat: Post Vs Get

--- site.php
+++ site.php
@@ -7,21 +7,13 @@
 </head>
 <body>
 
-    <form action="site.php" method="get">
-        Color: <input type="text" name="color"> <br>
-        Plural Noun: <input type="text" name="pluralNoun"> <br>
-        Celebrity: <input type="text" name="celebrity"> <br>
+    <form action="site.php" method="post">
+        Password: <input type="password" name="password">
         <input type="submit">
     </form>
 
     <?php   
-            $color = $_GET["color"];
-            $pluralNoun = $_GET["pluralNoun"];
-            $celebrity = $_GET["celebrity"];
-            
-            echo "Roses are $color <br>";
-            echo "$pluralNoun are blue <br>";
-            echo "I love $celebrity <br>";
+        echo $_POST["password"];
     
     ?>
    
