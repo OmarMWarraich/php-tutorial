3e60615ab5dc420421ea45bdb4c4896509da5d47
OmarMWarraich <earthling986@gmail.com>
2023-07-23 13:00:19 +0500

    feat: Functions

--- site.php
+++ site.php
@@ -7,14 +7,14 @@
 </head>
 <body>
 
-    <form action="site.php" method="post">
-        <input type="text" name="student">
-        <input type="submit">
-    </form>
-
     <?php
-        $grades = array("Jim"=>"A+", "Pam"=>"B-", "Oscar"=>"C+");
-        echo $grades[$_POST["student"]];
+        function sayHi($name, $age){
+            echo "Hello $name, you are $age <br>";
+        }
+
+        sayHi("Tom", 40);
+        sayHi("Dave", 30);
+        sayHi("Oscar", 20);
     ?>
    
 </body>
